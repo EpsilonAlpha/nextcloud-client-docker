c6184018dcc46289cbbb59060c63870814710e59
James Stout <stoutyhk@gmail.com>
2020-04-13 21:09:02 +0800

    Added ability to provide exclude and unsyncedfolders files
    Signed-off-by: James Stout <stoutyhk@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,16 @@
 FROM alpine:latest
-LABEL maintainer="juanignacioborda@gmail.com"
+LABEL maintainer="stoutyhk" version="0.1" description="nextcloud sync client"
+LABEL based-on="https://github.com/juanitomint/nextcloud-client-docker"
 ARG VCS_REF
 ARG BUILD_DATE
-ARG buildno
+
 ARG USER=ncsync
+ARG GROUP=users
 ARG USER_UID=1000
-ARG USER_GID=1000
+ARG USER_GID=100
 
 ENV USER=$USER \
+    GROUP=$GROUP \
     USER_UID=$USER_UID \
     USER_GID=$USER_GID \
     NC_USER=username \
@@ -15,19 +18,22 @@ ENV USER=$USER \
     NC_INTERVAL=500 \
     NC_URL="" \
     NC_TRUST_CERT=false \
-    NC_SOURCE_DIR="/media/nextcloud/" \
     NC_SILENT=false \
     NC_EXIT=false   \
-    NC_HIDDEN=false
+    NC_HIDDEN=false \
+    NC_MAX_SYNC_RETRIES=3
 
 
-# create group and user
-RUN addgroup -g $USER_GID $USER && adduser -G $USER -D -u $USER_UID $USER
+# create user
+RUN adduser -G $GROUP -D -u $USER_UID $USER
 
 # update repositories and install nextcloud-client
 RUN apk update && apk add nextcloud-client && rm -rf /etc/apk/cache
 
 # add run script
 ADD run.sh /usr/bin/run.sh
+
+VOLUME [ "/media/nextcloud" ]
+VOLUME [ "/config" ]
 
 CMD /usr/bin/run.sh
--- run.sh
+++ run.sh
@@ -1,26 +1,69 @@
-#!/bin/sh
+#!/bin/ash
+# shellcheck shell=bash
 
 # ensure, that the sync dir exists and is owned by the user
-[ -d $NC_SYNC_DIR ] || mkdir -p $NC_SYNC_DIR
-	
+[ -d /media/nextcloud ] || mkdir -p /media/nextcloud
+chown -R "$USER":"$GROUP" /media/nextcloud
+
+[ -d /config ] || mkdir -p /config
+chown -R "$USER":"$GROUP" /config
+
+# chmod -R 777 /media/nextcloud	
+
+file_exists() {
+	if [ -e "$1" ]; then
+		return 0
+	fi
+	return 1
+}
+
+# check exclude file exists
+if file_exists "/config/exclude"; then
+	EXCLUDE="--exclude /config/exclude"
+fi
+
+# check unsyncedfolders file exists
+if file_exists "/config/unsyncedfolders"; then
+	UNSYNCEDFOLDERS="--unsyncedfolders /config/unsyncedfolders"
+fi
+
+#construct command
+CMD="/bin/su -s /bin/ash "$USER" -c 'nextcloudcmd "
+
+if [ "$NC_HIDDEN" == true ]; then
+	CMD="${CMD} -h"
+fi
+if [ "$NC_SILENT" == true ]; then
+	CMD="${CMD} --silent"
+fi
+if [ "$NC_TRUST_CERT" == true ]; then
+	CMD="${CMD} --trust"
+fi
+
+if [ -n "${EXCLUDE}" ]; then
+	CMD="${CMD} ${EXCLUDE}"
+fi
+if [ -n "${UNSYNCEDFOLDERS}" ]; then
+	CMD="${CMD} ${UNSYNCEDFOLDERS}"
+fi
+if [ -n "${NC_MAX_SYNC_RETRIES}" ]; then
+	CMD="${CMD} --max-sync-retries ${NC_MAX_SYNC_RETRIES}"
+fi
+
+CMD="${CMD} --non-interactive -u $NC_USER -p $NC_PASS /media/nextcloud $NC_URL'"
 
 while true
 do
 
-	nextcloudcmd $( [ "$NC_HIDDEN" == true ] && echo "-h" ) $( [ "$NC_SILENT" == true ] && echo "--silent" ) $( [ "$NC_TRUST_CERT" == true ] && echo "--trust" ) --non-interactive -u $NC_USER -p $NC_PASS $NC_SOURCE_DIR $NC_URL
-	
-	#chown the files to the USER_UID:
-	echo "chown -R $USER_UID:$USER_GID $NC_SOURCE_DIR";
-	chown -R $USER_UID:$USER_GID $NC_SOURCE_DIR
-	
+	eval "$CMD"
 	#check if exit!
-		if [ "$NC_EXIT" = true ] ; then
-			
-			if [  ! "$NC_SILENT" == true ] ; then 
-				echo "NC_EXIT is true so exiting... bye!"
-			fi
-			
-			exit;
+	if [ "$NC_EXIT" = true ] ; then
+		
+		if [  ! "$NC_SILENT" == true ] ; then 
+			echo "NC_EXIT is true so exiting... bye!"
 		fi
-	sleep $NC_INTERVAL
+		
+		exit;
+	fi
+	sleep "$NC_INTERVAL"
 done
